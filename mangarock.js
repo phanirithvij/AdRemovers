6d78e40a03073f821954b5342dbd7c5947555214
phanirithvij <phanirithvij20003@gmail.com>
2018-07-10 20:52:08 +0530

    Added MangaRock.js

---
+++ mangarock.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name         MangaRock adremover
+// @namespace    https://github.com/phanirithvij/AdRemovers
+// @version      0.1
+// @description  Removes ads from MangaRock every 3 seconds
+// @author       Phani Rithvij
+// @match        https://mangarock.com/*
+// @include      https://mangarock.com/*/*
+// @include      https://mangarock.com/*/*/*
+// @include      https://mangarock.com/*/*/*/*
+// @include      https://mangarock.com/*/*/*/*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var removeAds = ()=>{
+        //Mangarock adblock (ad_remover to be precise)
+        document.querySelectorAll('iframe').forEach(e=>{
+            e.parentNode.removeChild(e);
+        });
+        var ad_divs = document.querySelectorAll('._3ccJK');
+        ad_divs.forEach(e=>{
+            e.parentNode.removeChild(e);
+        });
+        console.log("Ads removed");
+    };
+    window.setInterval(removeAds, 3000);
+    removeAds();
+})();
